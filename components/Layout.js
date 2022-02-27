adf20570a46dff25bc561568222e2dbf42dafe49
htanuma0313l <hinos.mac01@gmail.com>
2022-02-27 13:24:37 +0900

    footer修正２

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, title = "Default title" }) {
         {children}
       </main>
       <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
-        @kaba 2021
+        @kaba 2022
       </footer>
     </div>
   );
